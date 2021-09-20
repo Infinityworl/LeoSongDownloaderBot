a8e7548117c304f14f4ca008a2a05ebe18ca4203
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-09-21 01:35:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,13 @@ import os
 API_ID = int(os.getenv("API_ID", 2158704))
 API_HASH = os.getenv("API_HASH", "227f3bd8c1d7fc3ecfa243e1a85dd2fa")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "1996816091:AAFDEHO2Cadmg9zpZ_rQ5cLK231Fy_koQgE")
-UPDATES_CHANNEL = int(os.environ.get("UPDATES_CHANNEL", -1001231683570))
+UPDATES_CHANNEL = -1001231683570
 BOT_USERNAME = "leosongdownloaderbot"
-SESSION_NAME = os.environ.get("SESSION_NAME", "LeoSongDownloaderBot")
-BOT_OWNER = int(os.environ.get("BOT_OWNER", 1069002447))
+SESSION_NAME = "LeoSongDownloaderBot"
+BOT_OWNER = 1069002447
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://navindu:navi18572@cluster0.9yrur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001511224747))
-ARQ_API_KEY = os.getenv("ARQ_API_KEY", "RZOGUI-VCYTJC-FBTHZH-KWCYGT-ARQ")
-
+MONGODB_URI = "mongodb+srv://navindu:navi18572@cluster0.9yrur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+LOG_CHANNEL = -1001511224747
+ARQ_API_KEY = "RZOGUI-VCYTJC-FBTHZH-KWCYGT-ARQ"
 
 
