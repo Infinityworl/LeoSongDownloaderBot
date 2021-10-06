6fa70b1e84d645f1bc1ca3ba2510ac2056697c48
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-10-06 18:59:06 +0530

    Update __main__.py

--- LeoSongDownloaderBot/__main__.py
+++ LeoSongDownloaderBot/__main__.py
@@ -1,26 +1,9 @@
 # Leo Projects <https://t.me/leosupportx>
 # @Naviya2 🇱🇰
 
-import os
-import time
-import psutil
-import shutil
-import string
-import asyncio
-import config
 from pyromod import listen
-from asyncio import TimeoutError
 from LeoSongDownloaderBot import Bot
-from LeoSongDownloaderBot.translation import Translation
-from helper.database.access_db import db
-from helper.broadcast import broadcast_handler
-from helper.database.add_user import AddUserToDatabase
-from helper.display_progress import humanbytes, progress_for_pyrogram
-from pyrogram import Client
-from helper.forcesub import ForceSub
-from pyrogram.errors import FloodWait, UserNotParticipant
-from pyrogram import idle, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
 
 
 app = Bot()
