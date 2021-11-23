29c52a4caf3c59796f8e58e0f69f1fe7397bbc77
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-11-23 18:16:06 +0530

    done

--- helper/database/add_user.py
+++ helper/database/add_user.py
@@ -10,5 +10,5 @@ async def AddUserToDatabase(bot: Client, cmd: Message):
         if config.LOG_CHANNEL is not None:
             await bot.send_message(
                 int(config.LOG_CHANNEL),
-                f"#NEW_USER: \n\nNew User [{cmd.chat.id}](tg://user?id={cmd.chat.title}) started @{(await bot.get_me()).username} !!"
+                f"#NEW_USER: \n\nNew User [{cmd.chat.title}](tg://user?id={cmd.chat.id}) started @{(await bot.get_me()).username} !!"
             )
