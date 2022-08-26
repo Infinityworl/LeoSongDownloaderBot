ac5de41794f97f9796cbd3811b85dc6f56f4a299
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2022-08-26 21:58:09 +0530

    Updated Arq Api Key

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ BOT_OWNER = int(os.environ.get("BOT_OWNER", 1069002447))
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://navindu:navi18572@cluster0.9yrur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001511224747))
-ARQ_API_KEY = os.getenv("ARQ_API_KEY", "NSGHIX-URUHDE-QDSGPU-KLXQZQ-ARQ")
+ARQ_API_KEY = os.getenv("ARQ_API_KEY", "CKFTZZ-JANPYD-IAFUIP-SLJSLG-ARQ")
 TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BQAroNvCPX9Kd-WUEoNd6mgCl-GFnP4Bd_hsJNL5vJsI4-vWq6NhckEiwqMrHyepnTTsLFLLjniixXm0vabgm7asDhTgZ9OFAmzG5_mLQ29N8kwZy9F-MQG-oBvaBnTX6DVRyNoujKiB9qz_ox0ZIX5nWo_1kAgKFYJpmLy7MoRBSNRuK0hFB1ap2ZybwTaNu79hzXok8uQ44DG6aVnWD4xCKul6tGsxAiczitNeXyn52shzXXPR7KF_G53pAbBRD6M8Y41DNUEd1mQoRvXoXhpBmCCNAe7DG89eSpBJtBiAMgNBi9gzO8UziLg17ApBTzuH_y-e-6ctfbrXVOF5jOSQcgh0jgA")
 MAINCHANNEL_ID = -1001571768793
