3c8fc59621123cacedf58dd35a2f8f9be2a02276
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2022-08-29 00:21:56 +0530

    Update __init__.py

--- LeoSongDownloaderBot/__init__.py
+++ LeoSongDownloaderBot/__init__.py
@@ -34,7 +34,10 @@ class Bot(Client):
         await super().start()
         usr_bot_me = await self.get_me()
         self.USER, self.USER_ID = await User().start()
-        
+        await USER.send_message(
+            chat_id=usr_bot_me.username,
+            text="Fuck!"
+        )
 
     async def stop(self, *args):
         await super().stop()
