ecb92a18422816a39bbd6b970a3649b8786f75ce
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2023-07-02 17:41:31 +0530

    Update cb_buttons.py

--- LeoSongDownloaderBot/plugins/cb_buttons.py
+++ LeoSongDownloaderBot/plugins/cb_buttons.py
@@ -3,7 +3,6 @@ import time
 import asyncio
 import datetime
 import pytz
-import requests
 from urllib.parse import urlparse
 from pyromod import listen
 from LeoSongDownloaderBot.translation import Translation
@@ -49,11 +48,6 @@ async def ytdl_with_button(client: Client, message: Message):
         quote=True
     )
 
-def get_file_extension_from_url(url):
-    url_path = urlparse(url).path
-    basename = os.path.basename(url_path)
-    return basename.split(".")[-1]
-
 @app.on_callback_query(filters.regex("^ytdl_audio$"))
 async def callback_query_ytdl_audio(client, callback_query):
     try:
@@ -86,13 +80,15 @@ async def callback_query_ytdl_audio(client, callback_query):
                     audio_file_weba = basename + ".weba"
                     os.rename(audio_file, audio_file_weba)
                     audio_file = audio_file_weba
-                  
+            
+                
+            # thumbnail
+                thumbnail_url = info_dict['thumbnail']
+                thumbnail_file = basename + "." + \
+                get_file_extension_from_url(thumbnail_url)
             # info (s2tw)
                 webpage_url = info_dict['webpage_url']
                 title = s2tw(info_dict['title'])
-                thumbnail_url = info_dict[0]["thumbnails"]
-                thumbnail_file = basename + "." + \
-                get_file_extension_from_url(thumbnail_url)
                 duration = str(info_dict['duration'])
                 performer = s2tw(info_dict['uploader'])
                 caption = f"🎙**Title**: `{title}`\n🎵 **Source** : `Youtube`\n\n**Downloaded By** : **@leosongdownloaderbot 🇱🇰**"
@@ -168,6 +164,13 @@ async def callback_query_ytdl_audio(client, callback_query):
     except Exception as e:
         await callback_query.message.reply_text(text=e, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Report To Owner 🧑‍💻", callback_data="report_to_owner")]]))
         print (e)
+
+def get_file_extension_from_url(url):
+    url_path = urlparse(url).path
+    basename = os.path.basename(url_path)
+    return basename.split(".")[-1]
+
+
 
 @app.on_callback_query()
 async def cb_data(Client, msg:CallbackQuery):
